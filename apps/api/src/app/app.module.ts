62676c6e1f2956096c043954d3e9b68e7ef9495e
juanLif3 <juanitololilifeloli@gmail.com>
2026-01-06 22:45:06 -0300

    INIT: workspace setup with nestjs and react ready

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './modules/orders/orders.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { CompaniesModule } from './modules/companies/companies.module';
+import { ExpensesModule } from './modules/expenses/expenses.module';
+import { FinanceModule } from './modules/finance/finance.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    OrdersModule,
+    AuthModule,
+    CategoriesModule,
+    CompaniesModule,
+    ExpensesModule,
+    FinanceModule,
+    ProductsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
